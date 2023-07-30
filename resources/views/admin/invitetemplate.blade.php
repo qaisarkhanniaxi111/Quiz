aa7d4f20ca3ea45285a1b0f3a5b9afa507663c8b
QaisarKhanNiazi <Qaisar.qk17@gmail.com>
2023-07-30 04:33:52 -0700

    g

---
+++ resources/views/admin/invitetemplate.blade.php
@@ -0,0 +1,9 @@
+Hi, <b>{{$first}}</b><br><br>
+
+Please use these credentials for login
+<br><br>
+<b>Username: {{$emailaddress}}</b>
+<br>
+<b>Password: {{$random}}</b>
+<br><br>
+<a href="http://returntoclassics.com/jingfang/login">Go to Portal</a>
---
+++ resources/views/admin/invitetemplate2.blade.php
@@ -0,0 +1,8 @@
+Hi, <b>{{$first}} {{$lastname}}</b><br><br>
+
+JingFang invited you for an Assessment,
+<br>
+Kindly, Visit Below link to complete the Assessment.
+<br><br>
+
+<a href="http://returntoclassics.com/jingfang/assessment?code={{$random}}">Go to Assessment</a>
